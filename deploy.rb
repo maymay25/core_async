a187e9ef964e5b58b94e340bd1515ac49c167b96
maymay25 <jeffrey6052@gmail.com>
2014-05-27 15:40:28 +0800

    perfect deploy output

--- deploy.rb
+++ deploy.rb
@@ -5,50 +5,47 @@ env = ENV['RACK_ENV']||'production'
 
 command, type = ARGV[0], ARGV[1]
 
+def ps_ef_grep(msg)
+  sleep 3
+  cmd = "ps -ef | grep #{msg}"
+  puts "\n******** #{cmd} ********"
+  system(cmd)
+end
+
 case type
 when 'workers'
   process_sum = (tmp=ARGV[2].to_i)>0 ? tmp : 1
   case command
   when 'start'
-  process_sum.times do
-    system("RACK_ENV=#{env} bundle exec ruby #{app_root}/config/sidekiq_workers.rb")
-    sleep 3
-    cmd = "ps -ef | grep sidekiq"
-    puts "\n******** #{cmd} ********"
-    system(cmd)
-  end
+    process_sum.times do
+      system("RACK_ENV=#{env} bundle exec ruby #{app_root}/config/sidekiq_workers.rb")
+    end
   when 'stop'
     #TODO
   when 'restart'
     #TODO
   end
+  ps_ef_grep('sidekiq')
 when 'web'
   case command
   when 'start'
     system("RACK_ENV=#{env} bundle exec ruby #{app_root}/config/sidekiq_web.rb")
-    sleep 3
-    cmd = "ps -ef | grep core_async/config/unicorn.rb"
-    puts "\n******** #{cmd} ********"
-    system(cmd)
   when 'stop'
     system("kill `cat #{app_root}/tmp/pids/core_async_web.pid`")
   when 'restart'
     system("kill -usr2 `cat #{app_root}/tmp/pids/core_async_web.pid`")
   end
-
+  ps_ef_grep('core_async/config/unicorn.rb')
 when 'schedule'
   case command
   when 'start'
     system("RACK_ENV=#{env} bundle exec clockworkd -c #{app_root}/config/sidekiq_schedule.rb --pid-dir=#{app_root}/tmp/pids --log-dir=#{app_root}/log --log start")
-    sleep 3
-    cmd = "ps -ef | grep sidekiq_schedule"
-    puts "\n******** #{cmd} ********"
-    system(cmd)
   when 'stop'
     system("RACK_ENV=#{env} bundle exec clockworkd -c #{app_root}/config/sidekiq_schedule.rb --pid-dir=#{app_root}/tmp/pids --log-dir=#{app_root}/log --log stop")
   when 'restart'
     system("RACK_ENV=#{env} bundle exec clockworkd -c #{app_root}/config/sidekiq_schedule.rb --pid-dir=#{app_root}/tmp/pids --log-dir=#{app_root}/log --log restart")
   end
+  ps_ef_grep('sidekiq_schedule')
 else
   msg = <<-EOF
 
