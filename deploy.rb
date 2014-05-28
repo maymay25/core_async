e9c38d71029ec254ed3740bc4bfb050723f0097e
maymay25 <jeffrey6052@gmail.com>
2014-05-28 12:12:00 +0800

    system_run

--- deploy.rb
+++ deploy.rb
@@ -22,6 +22,11 @@ def fetch_sidekiq_pid_files(path)
   pid_files.sort_by{|path| path.split('.')[-1].to_i }
 end
 
+def system_run(cmd)
+  system(cmd)
+  puts cmd
+end
+
 case type
 when 'sidekiq'
   process_sum = (tmp=ARGV[2].to_i)>0 ? tmp : 1
@@ -31,7 +36,7 @@ when 'sidekiq'
     pid_files = fetch_sidekiq_pid_files(pid_path)
     if pid_files.length > 0
       pid_files.each do |file|
-        system("kill `cat #{file}`")
+        system_run("kill `cat #{file}`")
       end
     else
       puts "there was no pid files found, maybe already stoped. check it yourself."
@@ -43,29 +48,29 @@ when 'sidekiq'
     if pid_sum > 0
       if process_sum > pid_sum
         pid_files.each do |file|
-          system("kill -usr2 `cat #{file}`")
+          system_run("kill -usr2 `cat #{file}`")
         end
         (pid_sum..(process_sum-1)).each do |n|
-          system("RACK_ENV=#{env} bundle exec ruby #{app_root}/config/sidekiq_workers.rb #{n}")
+          system_run("RACK_ENV=#{env} bundle exec ruby #{app_root}/config/sidekiq_workers.rb #{n}")
         end
       elsif process_sum < pid_sum
         cache_sum = 0
         pid_files.each do |file|
           if cache_sum < process_sum
-            system("kill -usr2 `cat #{file}`")
+            system_run("kill -usr2 `cat #{file}`")
             cache_sum += 1
           else
-            system("kill `cat #{file}`")
+            system_run("kill `cat #{file}`")
           end
         end
       elsif process_sum == pid_sum
         pid_files.each do |file|
-          system("kill -usr2 `cat #{file}`")
+          system_run("kill -usr2 `cat #{file}`")
         end
       end
     else
       process_sum.times do |n|
-        system("RACK_ENV=#{env} bundle exec ruby #{app_root}/config/sidekiq_workers.rb #{n}")
+        system_run("RACK_ENV=#{env} bundle exec ruby #{app_root}/config/sidekiq_workers.rb #{n}")
       end
     end
   end
@@ -73,21 +78,21 @@ when 'sidekiq'
 when 'web'
   case command
   when 'start'
-    system("RACK_ENV=#{env} bundle exec ruby #{app_root}/config/sidekiq_web.rb")
+    system_run("RACK_ENV=#{env} bundle exec ruby #{app_root}/config/sidekiq_web.rb")
   when 'stop'
-    system("kill `cat #{app_root}/tmp/pids/core_async_web.pid`")
+    system_run("kill `cat #{app_root}/tmp/pids/core_async_web.pid`")
   when 'restart'
-    system("kill -usr2 `cat #{app_root}/tmp/pids/core_async_web.pid`")
+    system_run("kill -usr2 `cat #{app_root}/tmp/pids/core_async_web.pid`")
   end
   ps_ef_grep('core_async/config/unicorn.rb')
 when 'schedule'
   case command
   when 'start'
-    system("RACK_ENV=#{env} bundle exec clockworkd -c #{app_root}/config/sidekiq_schedule.rb --pid-dir=#{app_root}/tmp/pids --log-dir=#{app_root}/log --log start")
+    system_run("RACK_ENV=#{env} bundle exec clockworkd -c #{app_root}/config/sidekiq_schedule.rb --pid-dir=#{app_root}/tmp/pids --log-dir=#{app_root}/log --log start")
   when 'stop'
-    system("RACK_ENV=#{env} bundle exec clockworkd -c #{app_root}/config/sidekiq_schedule.rb --pid-dir=#{app_root}/tmp/pids --log-dir=#{app_root}/log --log stop")
+    system_run("RACK_ENV=#{env} bundle exec clockworkd -c #{app_root}/config/sidekiq_schedule.rb --pid-dir=#{app_root}/tmp/pids --log-dir=#{app_root}/log --log stop")
   when 'restart'
-    system("RACK_ENV=#{env} bundle exec clockworkd -c #{app_root}/config/sidekiq_schedule.rb --pid-dir=#{app_root}/tmp/pids --log-dir=#{app_root}/log --log restart")
+    system_run("RACK_ENV=#{env} bundle exec clockworkd -c #{app_root}/config/sidekiq_schedule.rb --pid-dir=#{app_root}/tmp/pids --log-dir=#{app_root}/log --log restart")
   end
   ps_ef_grep('sidekiq_schedule')
 else
