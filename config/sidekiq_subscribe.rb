89233a99e83c68006344e57d8dae5420c76b25ea
maymay25 <jeffrey6052@gmail.com>
2014-06-03 15:46:47 +0800

    debug

--- config/sidekiq_subscribe.rb
+++ config/sidekiq_subscribe.rb
@@ -21,47 +21,47 @@ EventMachine.run do
 
     subscribe_album_updated_dj(channel)
 
-    subscribe_album_off(channel)
+    # subscribe_album_off(channel)
 
-    subscribe_track_on(channel)
+    # subscribe_track_on(channel)
 
-    subscribe_track_off(channel)
+    # subscribe_track_off(channel)
 
-    subscribe_album_resend(channel)
+    # subscribe_album_resend(channel)
 
-    subscribe_comment_created_dj(channel)
+    # subscribe_comment_created_dj(channel)
 
-    subscribe_comment_destroyed_rb(channel)
+    # subscribe_comment_destroyed_rb(channel)
 
-    subscribe_favorite_created_dj(channel)
+    # subscribe_favorite_created_dj(channel)
 
-    subscribe_following_created_rb(channel)
+    # subscribe_following_created_rb(channel)
 
-    subscribe_following_destroyed_rb(channel)
+    # subscribe_following_destroyed_rb(channel)
 
-    subscribe_message_created_dj(channel)
+    # subscribe_message_created_dj(channel)
 
-    subscribe_relay_created_rb(channel)
+    # subscribe_relay_created_rb(channel)
 
-    subscribe_audio_queue(channel)
+    # subscribe_audio_queue(channel)
 
-    subscribe_dig_status_to(channel)
+    # subscribe_dig_status_to(channel)
 
-    subscribe_update_track_pic_category(channel)
+    # subscribe_update_track_pic_category(channel)
 
-    subscribe_user_audit(channel)
+    # subscribe_user_audit(channel)
 
-    subscribe_announcements(channel)
+    # subscribe_announcements(channel)
 
-    subscribe_user_update_audit(channel)
+    # subscribe_user_update_audit(channel)
 
-    subscribe_messages_dj(channel)
+    # subscribe_messages_dj(channel)
 
-    subscribe_last_uptrack(channel)
+    # subscribe_last_uptrack(channel)
 
-    subscribe_subapp_created(channel)
+    # subscribe_subapp_created(channel)
 
-    subscribe_packapp_feedback(channel)
+    # subscribe_packapp_feedback(channel)
 
   end
 end
